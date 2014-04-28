ffdfb7ccdc26544cce01427a9f1635c271586189
vuongbv_56 <vuongbv_56@vnu.edu.vn>
2014-04-28 17:23:27 +0700

    Adding HomeActivity and CategoryActivity(just init) Change com.example.fels to com.framgia.fels.activities

--- fels/src/com/framgia/fels/activities/CategoryActivity.java
+++ fels/src/com/framgia/fels/activities/CategoryActivity.java
@@ -4,27 +4,20 @@ import com.example.fels.R;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class CategoryActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
-		Intent i = new Intent(this, DatabaseTest.class);
-		startActivity(i);
-
-		// Intent intent = new Intent(this, HomeActivity.class);
-		// startActivity(intent);
+		setContentView(R.layout.activity_category);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.category, menu);
 		return true;
 	}
 
